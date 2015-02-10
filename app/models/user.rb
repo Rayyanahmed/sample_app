c8056c00bd24611073a6d2c563ae16d527f5261a
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2015-02-10 17:14:40 -0500

    First Commit

---
+++ app/models/user.rb
@@ -0,0 +1,80 @@
+class User < ActiveRecord::Base
+	has_many :microposts, dependent: :destroy 
+	attr_accessor :remember_token, :activation_token, :reset_token
+	before_save :downcase_email 
+	before_create :create_activation_digest
+	validates :name, presence: true, length: { maximum: 50 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255 },
+						format: { with: VALID_EMAIL_REGEX },
+						uniqueness: { case_sensitive: false }
+	has_secure_password
+	validates :password, length: { minimum: 6 }, allow_blank: true 
+
+
+	# Returns the hash digest of the given string. 
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+													  BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
+
+	# Return a random roken. 
+	def User.new_token 
+		SecureRandom.urlsafe_base64
+	end
+
+	# Remembers a user in the database for use in persistence sessions
+	def remember
+		self.remember_token = User.new_token
+		self.update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+	# Returns true if the given token matches the digest.
+	def authenticated?(attribute, token)
+		digest = self.send("#{attribute}_digest")
+		return false if digest.nil?
+		BCrypt::Password.new(digest).is_password?(token)
+	end
+
+	def activate
+		self.update_attribute(:activated, true)
+		self.update_attribute(:activated_at, Time.zone.now)
+	end
+
+	def send_activation_email
+		UserMailer.account_activation(self).deliver_now
+	end
+
+	def create_reset_digest
+		self.reset_token = User.new_token
+		update_attribute(:reset_digest, User.digest(reset_token))
+		update_attribute(:reset_sent, Time.zone.now)
+	end
+
+	# Forgets a user
+	def forget
+		update_attribute(:remember_digest, nil)
+	end
+
+	def send_password_reset_email
+		UserMailer.password_reset(self).deliver_now
+	end
+
+	def password_reset_expired?
+		reset_sent_at < 2.hours.ago 
+	end
+
+	private
+	# Creates and assigns teh activation token and digest.
+	def create_activation_digest 
+		self.activation_token = User.new_token
+		self.activation_digest = User.digest(activation_token)
+	end
+
+	
+
+	def downcase_email
+		self.email = email.downcase 
+	end
+end
